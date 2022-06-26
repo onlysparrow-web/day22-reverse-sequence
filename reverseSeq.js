a9dddb9214a7f80da6a055b79d8872f5467824ff
onlysparrow-web <104550421+onlysparrow-web@users.noreply.github.com>
2022-06-26 18:00:39 +0530

    Add files via upload

---
+++ reverseSeq.js
@@ -0,0 +1,12 @@
+let n = parseInt(prompt("enter the number"));
+let j = 1;
+let output = [];
+for (n; n >= j; n--) {
+  function reverseSequence(n) {
+    output.push(n);
+    return output;
+  }
+  var x = reverseSequence(n);
+}
+
+console.log(x);
